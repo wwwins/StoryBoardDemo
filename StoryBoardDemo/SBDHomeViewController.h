70e53042dde12eaca101787919bfc40ca78c8c97
wwwins <jacky.huang@isobar.com>
2014-02-20 15:08:19 +0800

    Added a home view controller.

---
+++ StoryBoardDemo/SBDHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  SBDHomeViewController.h
+//  StoryBoardDemo
+//
+//  Created by wwwins on 2014/2/12.
+//  Copyright (c) 2014年 wwwins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBDHomeViewController : UITableViewController
+
+@end
